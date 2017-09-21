802002eb4ed90bdcfe12205f6b79bc4b23cd66d8
michal <wami12@gmail.com>
2017-09-21 17:31:31 +0200

    Comments

--- GUI/source_UI/toolbar_items.js
+++ GUI/source_UI/toolbar_items.js
@@ -5,7 +5,7 @@
 //  Distributed under the terms of the BSD License.
 // To see where are all extensions loaded use:
 // 'jupyter notebook list'
-// look also:
+// look also (example dir):
 // '/home/michal/.local/share/jupyter/nbextensions/'
 // ---------------------------------------------------------------------------
 // Toobar icons adding
