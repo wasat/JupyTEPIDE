728fff4b4c6c1b77c10f33bcc2676071b5e8cab2
michal <wami12@gmail.com>
2017-09-21 17:31:04 +0200

    Code optimalization, functions for creating menu

--- GUI/source_UI/menu.js
+++ GUI/source_UI/menu.js
@@ -3,7 +3,12 @@
 // Copyright (C) 2017 .....
 //
 //  Distributed under the terms of the BSD License.
+//  To see where are all extensions loaded use:
+// 'jupyter notebook list'
+// look also (example dir):
+// '/home/michal/.local/share/jupyter/nbextensions/'
 // ---------------------------------------------------------------------------
+
 // Main menu positions adding
 //
 // - Przykład tworzenia menu głównego
@@ -35,81 +40,86 @@ define([
             , IPython._target);
     }
 
-    //tworzenie pozycji w menu głownym, przypisanie akcji
-    function create_menu () {
+    //tymczasowo, do testowania
+    function onClickEmpty() {
+        //evt.preventDefault(); //to jest animacja samego menu
+        //W tym miejscu umieszczamy wywołanie funkcji robiacej cos w Jupyterze notebook;
+        //open_notebook('moj_probny.ipynb');
+    }
 
+//albo zrobić sobie menu item definiowany w ten sposób jako obiekt - może on być parametrem wejściowym dla funkcji tworzącej menu:
+    function menu_item(klasa,href,text){
+        this.CSSclass=klasa;
+        this.itemHref=href;
+        this.itemText=text;
+    }
+//albo użyć kodu poniżej i przystosować go tylko do możliwości dodawania pozycji menu i nazywania
+// ich (mam tu 2 rodzaje menu, dodać jeszcze jedną)
+//Dodać do jupytePYDE to co już mam działające. Potem się będzie zmieniać kod, ale dodatek i plik startowy będzie ten sam
+//zacząć od rozbicia create_menu() na oddzielne funkcje tworzące różne rodzaje pozycji menu. Potem ustalić parametry wejściowe.
 
-        //elementy dodane do istniejacej pozycji "Help"
-        var moje_menu_item = $('<li/>')
+    //***
+    function add_submenu(name,appendToItemName){
+        var menu_item = $('<li/>')
             .addClass('dropdown-submenu')
             .append(
                 $('<a href="#">')
-                    .text('Moje menu')
+                    .text(name)
                     .on('click', function (evt) { evt.preventDefault(); })
             )
-            .appendTo($('#help_menu')); //id elementu w istniejacym menu glownym
+            .appendTo($(appendToItemName));
 
-        var moje_submenu = $('<ul/>')
+        var submenu = $('<ul/>')
             .addClass('dropdown-menu')
-            .appendTo(moje_menu_item);
+            .appendTo(menu_item);
 
+        return submenu;
+    }
+
+    function add_menu_item(name,desc,appendToMenu,onClickFn){
         $('<li/>')
-            .attr('title', 'Opis elementu 1')
+            .attr('title', desc)
             .append(
                 $('<a href="#">')
-                    .text('Element 1')
-                    .on('click', function (evt) {
-                        evt.preventDefault(); //to jest animacja samego menu
-                        //W tym miejscu umieszczamy wywołanie funkcji robiacej cos w Jupyterze notebook;
-                        //open_notebook('moj_probny.ipynb');
-                    })
+                    .text(name)
+                    .on('click', onClickFn)
             )
-            .appendTo(moje_submenu);
+            .appendTo(appendToMenu);
+    }
 
-        $('<li/>')
-            .attr('title', 'Opis elementu 2')
-            .append(
-                $('<a href="#">')
-                    .text('Element 2')
-                    .on('click', function (evt) {
-                        evt.preventDefault();
-                        //W tym miejscu umieszczamy wywołanie funkcji robiacej cos w Jupyterze notebook;
-                    })
-            )
-            .appendTo(moje_submenu);
+    //***
+//CZY MOŻNA PRZEKAZAĆ DOWOLNĄ FUNKCJĘ JAKO PARAMETR?
+    //tworzenie pozycji w menu głownym, przypisanie akcji
 
-        $('<li/>')
-            .attr('title', 'Opis elementu 3')
-            .append(
-                $('<a href="#">')
-                    .text('Element 3')
-                    .on('click', function (evt) {
-                        evt.preventDefault();
-                        //W tym miejscu umieszczamy wywołanie funkcji robiacej cos w Jupyterze notebook;
-                    })
-            )
-            .appendTo(moje_submenu);
+    function create_menu () {
+        //elementy dodane do istniejacej pozycji "Help"
+        moje_submenu = add_submenu('Moje menu 1','#help_menu')
+        add_menu_item('Element 1 moj','Opis elementu 1 moj',moje_submenu,function (evt){
+            evt.preventDefault();
+            open_notebook('moj_probny.ipynb');
+        });
 
-        $('<li/>')
-            .attr('title', 'Opis elementu 4')
-            .append(
-                $('<a href="#">')
-                    .text('Element 4')
-                    .on('click', function (evt) {
-                        evt.preventDefault();
-                        //W tym miejscu umieszczamy wywołanie funkcji robiacej cos w Jupyterze notebook;
-                    })
-            )
-            .appendTo(moje_submenu);
+        add_menu_item('Element 2 moj','Opis elementu 2 moj',moje_submenu,function (evt){
+            evt.preventDefault();
+        });
 
+        add_menu_item('Element 3 moj','Opis elementu 3 moj',moje_submenu,function (evt){
+            evt.preventDefault();
+        });
+
+        add_menu_item('Element 4 moj','Opis elementu 4 moj',moje_submenu,function (evt){
+            evt.preventDefault();
+        });
+
+        hr="http://www.wp.pl";
         $('<li/>')
             .attr('title', 'Idz do wp.pl')
             .append(
-                $('<a href="http://www.wp.pl">')
+                $('<a href='+hr+'>')
                     .text('www.wp.pl')
                     .on('click', function (evt) {
                         //evt.preventDefault();
-                        //W tym miejscu umieszczamy wywołanie funkcji robiacej cos w Jupyterze notebook;
+                        //jak chcemy odpalić href, to onclick musi być pusty, jak tu;
                     })
             )
             .appendTo(moje_submenu);
@@ -128,7 +138,6 @@ define([
 
         var moje_menu_item1 = $('<ul/>')
             .addClass('dropdown-menu')
-
             .appendTo($(nowe_menu_item)); //id elementu w istniejacym menu glownym
 
         $('<li/>')
