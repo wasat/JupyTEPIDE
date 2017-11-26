d27c81dc121ab99221fbad185a2feb36361e5614
Daniel Zinkiewicz <daniel@wasat.pl>
2017-11-26 23:42:32 +0100

    Changes in start script

--- Docker/start.sh
+++ Docker/start.sh
@@ -2,4 +2,4 @@
 
 docker run --name "postgis9421" -d -t kartoza/postgis:9.4-2.1
 docker run --name "geoserver9421"  --link postgis9421:postgis -p 8080:8080 -d -t kartoza/geoserver
-docker run -v /eodata:/eodata -it --rm --name "jupytepide" --link geoserver9421:geoserver -p 8888:8888 jupytepide/geospatial-notebook:latest start-notebook.sh --NotebookApp.token=''
+docker run -v /eodata:/eodata -it --rm --name "jupytepide" --link geoserver9421:geoserver -p 8888:8888 jupytepide/eodata-notebook:latest start-notebook.sh --NotebookApp.token=''
