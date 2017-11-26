f4a273c74cf34e381d5f3154e887d014d8d91025
Daniel Zinkiewicz <daniel@wasat.pl>
2017-11-26 22:47:50 +0100

    Add tags and mount of eodata repository

--- Docker/ver_1_3/build.sh
+++ Docker/ver_1_3/build.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+﻿#!/bin/sh
 
 cd 1_scipy-ext-notebook /
 sudo docker build -t jupytepide/scipy-ext-notebook:latest .
 
 cd ../2_geo-notebook/
-sudo docker build -t jupytepide/geospatial-notebook:latest  .
+sudo docker build -t jupytepide/geospatial-notebook:latest .
 
 cd ../3_eo-notebook/
 sudo docker build -t jupytepide/eodata-notebook:latest .
