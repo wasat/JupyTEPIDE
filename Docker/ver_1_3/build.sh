ea178a54d12fb8ec0dd05161d756926d5c62bfd4
Daniel Zinkiewicz <daniel@wasat.pl>
2017-11-26 16:13:52 +0100

    Add latest tag

--- Docker/ver_1_3/build.sh
+++ Docker/ver_1_3/build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 cd 1_scipy-ext-notebook /
-sudo docker build -t jupytepide/scipy-ext-notebook .
+sudo docker build -t jupytepide/scipy-ext-notebook:latest .
 
 cd ../2_geo-notebook/
-sudo docker build -t jupytepide/geospatial-notebook  .
+sudo docker build -t jupytepide/geospatial-notebook:latest  .
 
 cd ../3_eo-notebook/
-sudo docker build -t jupytepide/eodata-notebook .
+sudo docker build -t jupytepide/eodata-notebook:latest .
 
