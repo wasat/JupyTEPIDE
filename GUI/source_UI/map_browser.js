7826d58b17b4acdec459ae09cf85c266b87f7e56
michal <wami12@gmail.com>
2017-11-27 21:06:47 +0100

    Map browser first attempt

--- GUI/source_UI/map_browser.js
+++ GUI/source_UI/map_browser.js
@@ -1,2 +1,21 @@
 //można wykorzystać do animacji:
 //https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_slide_toggle
+
+define([
+    'base/js/namespace',
+    'jquery',
+    'require'
+], function (Jupyter,
+             $,
+             require) {
+
+    function load_extension(){
+      var map_panel = $('<div/>',{id:'map_panel', style:'width:20%'}).html('czarna krowa w kropki bordo jadła sobie trawę');
+      $('#ipython-main-app').append(map_panel);
+    };
+
+    // return public methods
+    return {
+        load_ipython_extension: load_extension
+    };
+});
