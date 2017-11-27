49f40f3f144f03b6e20c2da783b072d3517b20a5
Daniel Zinkiewicz <daniel@wasat.pl>
2017-11-27 03:25:49 +0100

    Numpy repair in python43 kernel

--- Docker/start.sh
+++ Docker/start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+docker stop geoserver9421 postgis9421 jupytepide
+docker rm geoserver9421 postgis9421 jupytepide
 
 docker run --name "postgis9421" -d -t kartoza/postgis:9.4-2.1
 docker run --name "geoserver9421"  --link postgis9421:postgis -p 8080:8080 -d -t kartoza/geoserver
