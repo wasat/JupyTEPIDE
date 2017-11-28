975dbf336b8e2f0ab99d8cd33b2d64f198a03e88
Daniel Zinkiewicz <zinkiewicz.daniel@gmail.com>
2017-11-28 14:37:23 +0100

    Add Jupyter Lab run script

--- Docker/lab_start.sh
+++ Docker/lab_start.sh
@@ -4,4 +4,4 @@ docker rm geoserver9421 postgis9421 jupytepide
 
 docker run --name "postgis9421" -d -t kartoza/postgis:9.4-2.1
 docker run --name "geoserver9421"  --link postgis9421:postgis -p 8080:8080 -d -t kartoza/geoserver
-docker run -v /eodata:/eodata -it --rm --name "jupytepide" --link geoserver9421:geoserver -p 8888:8888 jupytepide/eodata-notebook:latest start-notebook.sh --NotebookApp.token=''
+docker run -v /eodata:/eodata -it --rm --name "jupytepide" --link geoserver9421:geoserver -p 8888:8888 jupytepide/eodata-notebook:latest start.sh jupyter lab
