5b1072e5be38d6d8ef7df5bab132e11518e304f4
Daniel Zinkiewicz <daniel@wasat.pl>
2018-01-12 19:28:38 +0100

    Update before gdal bug repair

--- Docker/start.sh
+++ Docker/start.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-docker stop geoserver9421 postgis9421 jupytepide
-docker rm geoserver9421 postgis9421 jupytepide
+docker stop geoserver-ver_1.0 postgis-ver_1.0 jupytepide
+docker rm geoserver-ver_1.0 postgis-ver_1.0 jupytepide
 
-docker run --name "postgis9421" -d -t kartoza/postgis:9.4-2.1
-docker run --name "geoserver9421"  --link postgis9421:postgis -p 8080:8080 -d -t kartoza/geoserver
-docker run -v /eodata:/eodata -it --rm --name "jupytepide" --link geoserver9421:geoserver -p 8888:8888 jupytepide/eodata-notebook:latest start-notebook.sh --NotebookApp.token=''
+
+docker run --name "postgis-ver_1.0" -d -t jupytepide/kartoza-postgis:ver_1.0
+docker run --name "geoserver-ver_1.0"  --link postgis-ver_1.0:postgis -p 8080:8080 -d -t jupytepide/kartoza-geoserver:ver_1.0
+
+cd ~/JupyTEPIDE
+git pull
+
+sudo mount /eodata
+
+docker run -v /eodata:/eodata -v ~/JupyTEPIDE/notebooks:/home/jovyan/notebooks-local -it --rm --name "jupytepide" --link geoserver-ver_1.0:geoserver -p 8888:8888 jupytepide/eodata-notebook:ver_1.0.1 start-notebook.sh --NotebookApp.token=''
--- Docker/ver_1_3/build.sh
+++ Docker/ver_1_3/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 cd 1_scipy-ext-notebook /
-sudo docker build -t jupytepide/scipy-ext-notebook:latest .
+sudo docker build -t jupytepide/scipy-ext-notebook:ver_1.0.1 .
 
 cd ../2_geo-notebook/
-sudo docker build -t jupytepide/geospatial-notebook:latest .
+sudo docker build -t jupytepide/geospatial-notebook:ver_1.0.1 .
 
 cd ../3_eo-notebook/
-sudo docker build -t jupytepide/eodata-notebook:latest .
+sudo docker build -t jupytepide/eodata-notebook:ver_1.0.1 .
