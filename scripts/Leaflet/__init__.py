a406432efa40a6cf82520a9c8e48283f842514de
Jacek <jacek.rapinski@gmail.com>
2018-01-31 11:39:22 +0100

    Skeleton and first methods for a leaflet<->python wrapper

---
+++ scripts/Leaflet/__init__.py
@@ -0,0 +1,89 @@
+from IPython.display import HTML,display
+
+class Leaflet():
+
+    wmsAttribs={
+        'layers':'',
+        'styles':'',
+        'format':'image/png',
+        'transparent':True,
+        'version':'1.1.1',
+        'crs':'null',
+        'uppercase':False
+    }
+
+    params={
+        'color': 'red',
+        'fillColor': '#f03',
+        'fillOpacity': 0.5
+    }
+
+    def _attrib2string(self):
+        ls=[]
+        for k,v in self.wmsAttribs.items():
+            s = ''
+            if type(v)==bool:
+                val=str(v).lower()
+            else:
+                val='\''+v+'\''
+            s+=k+':'+val
+            ls.append(s)
+        s=','.join(ls)
+        return s
+
+    def _dict2string(self,d):
+        ls = []
+        for k, v in d:
+            s = ''
+            if type(v) == bool:
+                val = str(v).lower()
+            elif isinstance(v,str):
+                val = '\'' + v + '\''
+            else:
+                val = str(v)
+            s += k + ':' + val
+            ls.append(s)
+        s = ','.join(ls)
+        return s
+
+    def setView(self,B,L,zoom):
+        htm='''<script type="text/javascript">Jupytepide.map_setView([%f,%f],%f);</script>'''%(B,L,zoom)
+        display(HTML(htm))
+
+    def addRasterLayer(self): #TODO: dorobic rastra
+        pass
+
+    def addJsonLayer(self,j): #TODO: dorobic geojsona
+        pass
+
+    def addCircle(self,x,y,r,popup='',params=-1):
+        if params==-1:
+            params=self.params
+        htm = '''<script type="text/javascript">Jupytepide.map_addCircle([%f,%f],%f,%s,{%s});</script>''' % (x, y,r, popup,params)
+        display(HTML(htm))
+
+    def addMarker(self,x,y,popup='{title:\'Marker\', text:\'Marker\'}'):
+        htm = '''<script type="text/javascript">Jupytepide.map_addMarker([%f,%f],%s);</script>''' % (x, y, popup)
+        display(HTML(htm))
+
+    def addPolygon(self,x,y): #TODO: Dorobic polygona
+        pass
+
+    def addWmsLayer(self,url,name,attrib=-1):
+
+        if attrib==-1:
+            attrib=self._attrib2string()
+        elif isinstance(attrib,str):
+            pass
+        else:
+            attrib=self._dict2string(attrib)
+
+        htm = '''<script type="text/javascript">Jupytepide.map_addWmsLayer(%s,{%s},%s);</script>''' % (url, attrib, name)
+        display(HTML(htm))
+
+def Main():
+    ll=Leaflet()
+    print (ll._attrib2string())
+
+if __name__ == '__main__':
+    Main()
