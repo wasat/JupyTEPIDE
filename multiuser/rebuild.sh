41b4f00d65f3a93ad9ac43b69dba323776b70526
zidan <daniel.zinkiewicz@wasat.pl>
2018-02-12 17:15:35 +0100

    test dev

--- multiuser/rebuild.sh
+++ multiuser/rebuild.sh
@@ -4,8 +4,8 @@ docker-compose down
 docker rmi -f jupytepide-hub jupytepide-hub-user
 
 make build
-make notebook_image
-# make jupytep_user_notebook
+# make notebook_image
+make jupytep_user_notebook
 
 docker-compose up -d
 docker ps -a
