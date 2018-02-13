2fbccb029977d4fe79c12410b4b558a714ed513f
zidan <daniel.zinkiewicz@wasat.pl>
2018-02-13 12:30:53 +0100

    Clean multiuser folder

--- multiuser/rebuild.sh
+++ multiuser/rebuild.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 docker-compose down
-docker rmi -f jupytepide-hub jupytepide-hub-user
+docker rmi -f jupytepide-hub
+# docker rmi -f jupytepide-hub-user
 
 make build
 # make notebook_image
