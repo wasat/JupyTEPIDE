b93d90b7c4833f807e052fa660e57d50562ae495
Daniel Zinkiewicz <zinkiewicz.daniel@gmail.com>
2018-02-18 00:34:35 +0100

    Added shared and public volumes

--- multiuser/rebuild.sh
+++ multiuser/rebuild.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
 docker-compose down
-#docker rmi -f jupytepide-hub
+# docker rmi -f jupytepide-hub
 docker rmi -f jupytepide-hub-user
 
 # make build
-# make notebook_image
 make jupytep_user_spawn_image
 
 docker-compose up -d
