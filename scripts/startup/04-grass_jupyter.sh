a16334297f5a30084d6a324e2382cea3deffd917
Daniel Zinkiewicz <zinkiewicz.daniel@gmail.com>
2018-04-20 23:00:17 +0200

    Reverse wrong commits

--- scripts/startup/04-grass_jupyter.sh
+++ scripts/startup/04-grass_jupyter.sh
@@ -1,24 +1,19 @@
 # directory where GRASS GIS lives
 export GISBASE=`grass74 --config path`   # or define path to binaries like /usr/local/grass-7.4.svn
 echo $GISBASE
-
 # generate GISRC
 # Defines the system wide value while in a GRASS session
 MYGISDBASE=$HOME/grassdata # Change this path to reflect your own
 MYLOC=MyLoc # Change this location name to reflect your own
 MYMAPSET=PERMANENT
-
 # Set the global grassrc file to individual file name
 MYGISRC="$HOME/.grassrc.$$"
-
 echo "GISDBASE: $MYGISDBASE" > "$MYGISRC"
 echo "LOCATION_NAME: $MYLOC" >> "$MYGISRC"
 echo "MAPSET: $MYMAPSET" >> "$MYGISRC"
 echo "GRASS_GUI: text" >> "$MYGISRC"
-
 # path to GRASS settings file
 export GISRC=$MYGISRC
-
 export LD_LIBRARY_PATH=$GISBASE/lib:$LD_LIBRARY_PATH
 export PYTHONPATH=$GISBASE/etc/python:$PYTHONPATH
 export PATH=$GISBASE/bin:$GISBASE/scripts:$PATH
