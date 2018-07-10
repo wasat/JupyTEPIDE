4ab916eca70920be4b976aa94f6ad17ee607d8af
zidan <daniel.zinkiewicz@wasat.pl>
2018-07-10 13:24:27 +0200

    Clean Jupterhub files

--- multiuser/rebuild.sh
+++ multiuser/rebuild.sh
@@ -5,7 +5,7 @@ docker rmi -f jupytepide-hub
 # docker rmi -f jupytepide-hub-user
 
 # make build
-make jupytep_user_spawn_image
+# make jupytep_user_spawn_image
 
 docker-compose up -d
 docker ps -a
